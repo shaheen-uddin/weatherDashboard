81098f6e5609b6083afd3ed1f9ac3d5908a6598b
shaheen-uddin <shaheenelt@gmail.com>
2024-02-08 01:36:02 +0600

    VITE env error fixed

--- src/components/weather/WeatherDashboard.jsx
+++ src/components/weather/WeatherDashboard.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import AddToFavorite from "./AddToFavorite";
 import WeatherHeadline from "./WeatherHeadline";
 import WeatherCondition from "./WeatherCondition";
+import { useWheather } from "../../hooks";
 
 export default function WeatherDashboard() {
+   const {weatherData, loading, error} = useWheather();
+  console.log(weatherData);
+  console.log(loading);
+  console.log(error); 
+  console.log( import.meta.env.VITE_WEATHER_API_KEY);
+  console.log(import.meta.env.VITE_SOME_KEY);
   return (
-    <div class="container">
-      <div class="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
-        <div class="grid md:grid-cols-2 gap-10 md:gap-6">
+    <div className="container">
+      <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
+        <div className="grid md:grid-cols-2 gap-10 md:gap-6">
           <AddToFavorite />
           <WeatherHeadline />
           <WeatherCondition />
---
+++ src/hooks/index.js
@@ -0,0 +1,3 @@
+import useWheather from "./useWeather";
+
+export { useWheather };
