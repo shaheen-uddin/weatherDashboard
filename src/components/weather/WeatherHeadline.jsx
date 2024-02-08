5fa7209a60ef57540221f1ce399fd3ce9117ca68
shaheen-uddin <shaheenelt@gmail.com>
2024-02-08 13:49:57 +0600

    Main content completed

--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -1,14 +1,48 @@
 import { useContext } from "react";
+import CloudIcon from "../../assets/cloud.svg";
+import HazeIcon from "../../assets/haze.svg";
+import SnowIcon from "../../assets/icons/snow.svg";
+import SunnyIcon from "../../assets/icons/sunny.svg";
+import RainIcon from "../../assets/rainy.svg";
+import ThunderIcon from "../../assets/thunder.svg";
 import PinIcon from "../../assets/pin.svg";
 import { WeatherContext } from "../../context";
 import { getFormattedDate } from "../../utils/date-util";
 
 export default function WeatherHeadline() {
   const { weatherData } = useContext(WeatherContext);
+  console.log("TIIIIme: " + getFormattedDate(1707375885, "time", false));
+
+  function getWeatherIcon(climate) {
+    console.log("Icon: " + climate);
+    switch (climate) {
+      case "Rain":
+        return RainIcon;
+      case "Haze":
+        return HazeIcon;
+      case "Clouds":
+        return CloudIcon;
+      case "Clear":
+        return SunnyIcon;
+      case "Fog":
+        return HazeIcon;
+      case "Thunder":
+        return ThunderIcon;
+      case "Snow":
+        return SnowIcon;
+      case "Mist":
+        return HazeIcon;
+      default:
+        return SunnyIcon;
+    }
+  }
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src="./assets/cloud.svg" alt="cloud" />
+        <img
+          src={getWeatherIcon(weatherData.climate)}
+          alt={weatherData.climate}
+        />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
             {Math.floor(weatherData.temperature)}°
@@ -21,7 +55,7 @@ export default function WeatherHeadline() {
       </div>
       <p className="text-sm lg:text-lg">
         {getFormattedDate(weatherData.time, "time", false)} -
-        {getFormattedDate(weatherData.time, "date", false)}, 9 Dec ‘23
+        {getFormattedDate(weatherData.time, "date", false)}
       </p>
     </div>
   );
--- src/utils/date-util.js
+++ src/utils/date-util.js
@@ -1,4 +1,4 @@
-export function getFormattedDate(value, type, inMs){
+/* export function getFormattedDate(value, type, inMs){
   if(!type) return value;
   if(!inMs){
     return value = value * 1000;
@@ -15,8 +15,38 @@ export function getFormattedDate(value, type, inMs){
 } else if(type === 'time'){
     options = {
         hour: "numeric",
-        minute: "numeric"
+        minute: "numeric",
     }
 }
 return new Intl.DateTimeFormat("en-us", options).format(date);
+} */
+
+function getFormattedDate(value, type, inMS) {
+    if(!type) return value;
+
+    if (!inMS) {
+        value = value * 1000;
+    }
+
+    const date = new Date(value);
+    let options;
+
+    if (type === 'date') {
+        options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        };
+    } else if(type === 'time') {
+        options = {
+            hour: "numeric",
+            minute: "numeric",
+          };
+    }
+
+
+    return new Intl.DateTimeFormat("en-us", options).format(date);
 }
+
+export {getFormattedDate};
